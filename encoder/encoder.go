331a2c987d222da5db047b6f843b5b1de2983eef
Jack <ipchchv@gmail.com>
2022-02-06 14:36:36 +0300

    Add message encryption function

--- encoder/encoder.go
+++ encoder/encoder.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/rand"
 	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
+	var message string
 	key := getKey()
 	hashedKey := getHash(key)
+	fmt.Print("Enter the message: ")
+	if _, err := fmt.Fscan(os.Stdin, &message); err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("%s\n", encrypt(message, key))
+	fmt.Printf("Your secret key: %s\n", key)
 	fmt.Print(checkKey(key, hashedKey))
 }
 
@@ -34,4 +46,26 @@ func checkKey(key string, hashedKey string) bool {
 	} else {
 		return false
 	}
+}
+
+func encrypt(text string, keyString string) (encryptedText string) {
+	key, err := hex.DecodeString(keyString)
+	plaintext := []byte(text)
+	if err != nil {
+		panic(err.Error())
+	}
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err.Error())
+	}
+	aesGCM, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+	nonce := make([]byte, aesGCM.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
+	cipherText := aesGCM.Seal(nonce, nonce, plaintext, nil)
+	return fmt.Sprintf("%x", cipherText)
 }
