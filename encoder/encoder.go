2e2ba564427e71a0ebb924d51244a0684ba825b6
Jack <ipchchv@gmail.com>
2022-02-06 13:45:34 +0300

    Do some refactoring and add the key validation function

--- encoder/encoder.go
+++ encoder/encoder.go
@@ -7,23 +7,31 @@ import (
 )
 
 func main() {
-	bytesKey, key := getKey()
-	hashedKey := getHash(bytesKey)
-	fmt.Printf("key: %s\n", key)
-	fmt.Printf("hashed key: %s", hashedKey)
+	key := getKey()
+	hashedKey := getHash(key)
+	fmt.Print(checkKey(key, hashedKey))
 }
 
-func getKey() (bytes []byte, key string) {
-	bytes = make([]byte, 32)
+func getKey() (key string) {
+	bytes := make([]byte, 32)
 	if _, err := rand.Read(bytes); err != nil {
 		panic(err.Error())
 	}
 	key = fmt.Sprintf("%x", bytes)
-	return bytes, key
+	return key
 }
 
-func getHash(key []byte) (hashedKey string) {
-	bytes := sha256.Sum256(key)
+func getHash(key string) (hashedKey string) {
+	bytes := sha256.Sum256([]byte(key))
 	hashedKey = fmt.Sprintf("%x", bytes)
 	return hashedKey
+}
+
+func checkKey(key string, hashedKey string) bool {
+	hash := getHash(key)
+	if hash == hashedKey {
+		return true
+	} else {
+		return false
+	}
 }
