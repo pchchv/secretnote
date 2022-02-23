07083b161efab2d208fd1f321ffb43670353e0fe
pchchv <ipchchv@gmail.com>
2022-02-23 06:09:14 +0300

    Add a decryption function

--- processors/AESCipher.py
+++ processors/AESCipher.py
@@ -3,10 +3,12 @@ from Crypto import Random
 from Crypto.Cipher import AES
 from base64 import b64encode, b64decode
 
+
 class AESCipher(object):
     def __init__(self, key):
         self.block_size = AES.block_size
         self.key = hashlib.sha256(key.encode()).digest()
+
 
     def encrypt(self, plain_text):
         """Gets plain text to be encrypted and returns the encrypted text and the hashed key"""
@@ -15,6 +17,15 @@ class AESCipher(object):
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
         encrypted_text = cipher.encrypt(plain_text.encode())
         return b64encode(iv + encrypted_text).decode("utf-8"), b64encode(self.key).decode("utf-8")
+    
+
+    def decrypt(self, encrypted_text):
+        """Gets encrypted_text, returns plain_text"""
+        encrypted_text = b64decode(encrypted_text)
+        iv = encrypted_text[:self.block_size]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        plain_text = cipher.decrypt(encrypted_text[self.block_size:]).decode("utf-8")
+        return self.__unpad(plain_text)
 
 
     def __pad(self, plain_text):
@@ -27,3 +38,11 @@ class AESCipher(object):
         padding_str = number_of_bytes_to_pad * ascii_string
         padded_plain_text = plain_text + padding_str
         return padded_plain_text
+
+
+    @staticmethod
+    def __unpad(plain_text):
+        """Gets the decoded plain_text, and removes the characters added in the __pad method"""
+        last_character = plain_text[len(plain_text) - 1:]
+        bytes_to_remove = ord(last_character)
+        return plain_text[:-bytes_to_remove]
