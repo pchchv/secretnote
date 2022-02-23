d0a5e169e70dec623011d69503f55d80e568a7ac
pchchv <ipchchv@gmail.com>
2022-02-23 06:31:19 +0300

    Refactoring

--- processors/AESCipher.py
+++ processors/AESCipher.py
@@ -9,40 +9,43 @@ class AESCipher(object):
         self.block_size = AES.block_size
         self.key = hashlib.sha256(key.encode()).digest()
 
-
     def encrypt(self, plain_text):
-        """Gets plain text to be encrypted and returns the encrypted text and the hashed key"""
+        """Gets plain text to be encrypted and
+        returns the encrypted text and the hashed key
+        
+        """
         plain_text = self.__pad(plain_text)
         iv = Random.new().read(self.block_size)
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
         encrypted_text = cipher.encrypt(plain_text.encode())
-        return b64encode(iv + encrypted_text).decode("utf-8"), b64encode(self.key).decode("utf-8")
-    
+        return (b64encode(iv + encrypted_text).decode("utf-8"),
+                b64encode(self.key).decode("utf-8"))
 
     def decrypt(self, encrypted_text):
         """Gets encrypted_text, returns plain_text"""
         encrypted_text = b64decode(encrypted_text)
         iv = encrypted_text[:self.block_size]
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        plain_text = cipher.decrypt(encrypted_text[self.block_size:]).decode("utf-8")
+        plain_text = (cipher.decrypt(encrypted_text[self.block_size:])
+                      .decode("utf-8"))
         return self.__unpad(plain_text)
 
-
     def __pad(self, plain_text):
-        """Gets the plain_text to be encrypted 
-        and adds the necessary number of bytes to make it a multiple of 128 bits
+        """Gets the plain_text to be encrypted and
+        adds the necessary number of bytes to make it a multiple of 128 bits
 
         """
-        number_of_bytes_to_pad = self.block_size - len(plain_text) % self.block_size
+        number_of_bytes_to_pad = (self.block_size - len(plain_text)
+                                  % self.block_size)
         ascii_string = chr(number_of_bytes_to_pad)
         padding_str = number_of_bytes_to_pad * ascii_string
         padded_plain_text = plain_text + padding_str
         return padded_plain_text
 
-
     @staticmethod
     def __unpad(plain_text):
-        """Gets the decoded plain_text, and removes the characters added in the __pad method"""
+        """Gets the decoded plain_texе and
+        removes the characters added in the __pad method"""
         last_character = plain_text[len(plain_text) - 1:]
         bytes_to_remove = ord(last_character)
         return plain_text[:-bytes_to_remove]
