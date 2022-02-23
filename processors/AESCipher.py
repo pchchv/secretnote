b6172ebcf4acd6b52a357ab524094858e943ad99
pchchv <ipchchv@gmail.com>
2022-02-23 05:42:51 +0300

    Add text encryption function

---
+++ processors/AESCipher.py
@@ -0,0 +1,29 @@
+import hashlib
+from Crypto import Random
+from Crypto.Cipher import AES
+from base64 import b64encode, b64decode
+
+class AESCipher(object):
+    def __init__(self, key):
+        self.block_size = AES.block_size
+        self.key = hashlib.sha256(key.encode()).digest()
+
+    def encrypt(self, plain_text):
+        """Gets plain text to be encrypted and returns the encrypted text and the hashed key"""
+        plain_text = self.__pad(plain_text)
+        iv = Random.new().read(self.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        encrypted_text = cipher.encrypt(plain_text.encode())
+        return b64encode(iv + encrypted_text).decode("utf-8"), b64encode(self.key).decode("utf-8")
+
+
+    def __pad(self, plain_text):
+        """Gets the plain_text to be encrypted 
+        and adds the necessary number of bytes to make it a multiple of 128 bits
+
+        """
+        number_of_bytes_to_pad = self.block_size - len(plain_text) % self.block_size
+        ascii_string = chr(number_of_bytes_to_pad)
+        padding_str = number_of_bytes_to_pad * ascii_string
+        padded_plain_text = plain_text + padding_str
+        return padded_plain_text
